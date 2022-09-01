43e0f05309c6ebc50f773464ca72fbda9597d8c1
Herman Chan <69499714+Hermancwh@users.noreply.github.com>
2022-09-01 14:47:08 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cgt6ja4ssldq.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "chanwenhan-employee"
+customregion = "us-east-1"
